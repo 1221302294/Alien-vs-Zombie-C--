672c08da27739fa39166bd78302a6b856c5fb81e
1221302637 <123657438+CheeChoong14@users.noreply.github.com>
2023-02-18 11:38:02 +0800

    Update mainheader.h

--- mainheader.h
+++ mainheader.h
@@ -7,13 +7,24 @@
 // Emails: 1221302637@student.mmu.edu.my | 1221302294@student.mmu.edu.my      //
 // Phones: 010-2675333                   | 017-9325346                     //
 // ************************************************************************** //
-#include <vector> 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib> // for system()
+#include <ctime>   // for time() in srand( time(NULL) );
+#include <iomanip> // for setw()
 #ifndef MAINHEADER_H
 #define MAINHEADER_H
 
+// Global--------------------------------------------------------------------------------------- 
 
-    void ClearScreen(); // (from tutorial)
-    void Pausee(){};// (from tutorial)
+    
+    int ClearScreen(); // (from tutorial)
+    int Pause(); // (from tutorial)
+    // int move_up();
+
+    extern std::vector<std::vector<char>> map_; 
+    void emptymap(int dX, int dY);
     void displayMenu(); // cc (from tutorial)
     void controls(); // cc (from tutorial)
     void gameover(); // tingli
@@ -24,9 +35,27 @@
     void zombiestat(int no); // tingli
     void alienstat(); // tingli
     void settings();
-    void defplay();
+    char randObject(char x);
     void play();
 
+    void init(int dimX, int dimY, int zomb); 
+        void display(); 
+        int setDimX(int x); 
+        int setDimY(int y); 
+        int setZomB(int z); 
+        std::vector<std::vector<char>> setmap(int y, int x, char obj);
+
+        int getDimX();  
+        int getDimY();  
+        int getZomB(); 
+        char getObject(int dimX, int dimY); 
+
+    int *getZomHP(int num);
+    int *getZomAtk(int numZom);
+    int *getZomRge(int numofZ);
+    int fileExist();
+    void loadmap();
+//Global---------------------------------------------------------------------------------------
 //Class-----------------------------------------------------------------------------------------
 
 class gameBoard // cc&tingli (from tutorial)
@@ -69,18 +98,36 @@ class Zombie : public gameBoard
 class movement : public gameBoard
 {
     public:
+        int alhp_ = 100;
+        int alatk_ = 0;
         int AX_;
         int AY_;
 
+        void alienstat();
+        int getalhp_();
+        int setalhp_(int hp);
         int setaX_(int x);
         int setaY_(int y);
+        int getaX_() const;
+        int getaY_() const;
+
 
         void move_up();
         void move_down();
         void move_left();
         void move_right();
-        void move_arrow();
+        void move_arrow(int y, int x, string z);
         void get_arrow();
+};
+
+class Enemy // Afiq 
+{
+    public:
+        std::vector<int> ZHpVec;
+        std::vector<int> ZAtkVec;     
+        std::vector<int> ZRngVec;
+        void zombiegen(int no);
+
 };
 
 //Class-----------------------------------------------------------------------------------------
